e3c2b692da6490ab40ee8bf7d54f700dc55b1484
Seb Glazebrook <me@sebglazebrook.com>
2014-07-19 13:21:03 +1000

    Added rspec for testing

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative '../lib/kapture'
+
+ROOT_DIR = Pathname.new(File.expand_path('../..', __FILE__))
+
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+
+RSpec.configure do |config|
+  config.color = true
+  config.formatter     = 'documentation'
+end
+
+
+
